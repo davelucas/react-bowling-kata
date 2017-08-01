aa2170e0cf5b968be9b2f2d0c6870bd80333a47e
David Lucas <dlucas@thoughtworks.com>
2017-08-01 17:31:26 +0100

    An incomplete example

--- src/game/gameReducer.jsx
+++ src/game/gameReducer.jsx
@@ -11,6 +11,78 @@ let startOfGame = {
 
 export default function gameReducer(state = startOfGame, action) {
 
+  function firstRollInTheFrame(previousFrames) {
+    return previousFrames.isEmpty() || previousFrames.last().finished
+  }
+
+  function addRoll(frames, down) {
+    if (firstRollInTheFrame(frames)) {
+      let isStrike = down === 10;
+      return frames.push({
+        rolls: List.of(down),
+        strike: isStrike,
+        spare: false,
+        finished: isStrike
+      })
+    } else {
+      return frames.pop().push({
+        rolls: frames.last().rolls.push(down),
+        strike: false,
+        spare: frames.last().rolls.first() + down === 10,
+        finished: true
+      })
+    }
+  }
+
+  function isGameFinished() {
+    if (state.frames.size >= 12) {
+      return true
+    }
+
+    if (state.frames.size >= 10) {
+      let frameScore = state.frames.last().rolls.reduce((a, b) => a + b, 0);
+      return frameScore !== 10
+    }
+
+    return false
+  }
+
+  function calculateScores(frames) {
+    return frames.map((frame, i) => {
+
+      if (frame.spare) {
+        let nextFrame = frames.get(i + 1)
+        if (undefined !== nextFrame) {
+          return 10 + nextFrame.rolls.first()
+        } else {
+          return undefined
+        }
+      }
+
+      if (frame.strike) {
+        let nextFrame = frames.get(i + 1)
+
+        if (undefined !== nextFrame) {
+          let nextRoll = nextFrame.rolls.first()
+          if (nextFrame.rolls.size >= 2) {
+            return 10 + nextRoll + nextFrame.rolls.shift().first()
+          } else {
+            let secondNextFrame = frames.get(i + 2)
+            if (undefined !== secondNextFrame) {
+              return 10 + nextRoll + secondNextFrame.rolls.first()
+            } else {
+              return undefined
+            }
+          }
+        }
+      }
+
+      return frame.rolls.size === 2 ? frame.rolls.reduce((a, b) => a + b) : undefined
+    })
+      .filter(frameScore => undefined !== frameScore)
+      .reduce((r, frameScore) => r.push(r.isEmpty() ? frameScore : r.last() + frameScore), List())
+  }
+
   switch (action.type) {
     case START_BOWLING:
       return {
@@ -18,14 +90,17 @@ export default function gameReducer(state = startOfGame, action) {
         bowling: true
       }
     case BOWL_FINISHED:
+      let nextFrames = addRoll(state.frames, action.results.down)
       return {
-        ...state
-        // TODO: Hint use action.results.down in here to affect the scores and other state
+        ...state,
+        frames: nextFrames,
+        scores: calculateScores(nextFrames),
+        bowling: false
       }
     case FRAME_FINISHED:
       return {
-        ...state
-        // TODO: Implement anything that needs to happen when the frame is finished
+        ...state,
+        gameOver: isGameFinished()
       }
     case NEW_GAME:
       return {
--- src/game/gameReducer.spec.js
+++ src/game/gameReducer.spec.js
@@ -75,8 +75,437 @@ describe('gameReducer', () => {
     )
   })
 
-  // TODO: Add more tests here
-  // TODO: For example, handle the perfect game (all strikes), handle spares, handle game over etc
+  it('scores 20 for a game of all ones', () => {
+    let allOneFrame = {
+      rolls: List([1, 1]),
+      strike: false,
+      spare: false,
+      finished: true,
+    }
+
+    expect(
+      gameReducer(
+        {
+          frames: List.of(
+            allOneFrame,
+            allOneFrame,
+            allOneFrame,
+            allOneFrame,
+            allOneFrame,
+            allOneFrame,
+            allOneFrame,
+            allOneFrame,
+            allOneFrame,
+            {
+              rolls: List([1]),
+              strike: false,
+              spare: false,
+              finished: false
+            }
+          ),
+          scores: List.of(2,4,6,8,10,12,14,16,18),
+          bowling: false,
+          gameOver: false
+        }, {
+          type: actions.BOWL_FINISHED,
+          results: {
+            down: 1
+          }
+        })
+    ).toEqual(
+      {
+        frames: List.of(
+          allOneFrame,
+          allOneFrame,
+          allOneFrame,
+          allOneFrame,
+          allOneFrame,
+          allOneFrame,
+          allOneFrame,
+          allOneFrame,
+          allOneFrame,
+          allOneFrame
+        ),
+        scores: List.of(2,4,6,8,10,12,14,16,18,20),
+        bowling: false,
+        gameOver: false
+      }
+    )
+  })
+
+
+  it('adds the first roll', () => {
+    expect(
+      gameReducer(
+        {
+          frames: List(),
+          scores: List(),
+          bowling: false,
+          gameOver: false
+        }, {
+          type: actions.BOWL_FINISHED,
+          results: {
+            down: 3
+          }
+        })
+    ).toEqual(
+      {
+        frames: List.of(
+          {
+            rolls: List([3]),
+            strike: false,
+            spare: false,
+            finished: false
+          },
+        ),
+        scores: List(),
+        bowling: false,
+        gameOver: false
+      }
+    )
+  })
+
+  it('registers a spare if all the pins are knocked down in two rolls of a frame', () => {
+    expect(
+      gameReducer(
+        {
+          frames: List.of(
+            {
+              rolls: List([4]),
+              strike: false,
+              spare: false,
+              finished: false
+            }
+          ),
+          scores: List(),
+          bowling: false,
+          gameOver: false
+        }, {
+          type: actions.BOWL_FINISHED,
+          results: {
+            down: 6
+          }
+        })
+    ).toEqual(
+      {
+        frames: List.of(
+          {
+            rolls: List([4, 6]),
+            strike: false,
+            spare: true,
+            finished: true
+          }
+        ),
+        scores:  List.of(),
+        bowling: false,
+        gameOver: false
+      }
+    )
+  })
+
+  it('uses the value of the next roll after a spare as the spare bonus', () => {
+    expect(
+      gameReducer(
+        {
+          frames: List.of(
+            {
+              rolls: List([4, 6]),
+              strike: false,
+              spare: true,
+              finished: true
+            }
+          ),
+          scores: List.of(0),
+          bowling: false,
+          gameOver: false
+        }, {
+          type: actions.BOWL_FINISHED,
+          results: {
+            down: 3
+          }
+        })
+    ).toEqual(
+      {
+        frames: List.of(
+          {
+            rolls: List([4, 6]),
+            strike: false,
+            spare: true,
+            finished: true
+          },
+          {
+            rolls: List([3]),
+            strike: false,
+            spare: false,
+            finished: false
+          }
+        ),
+        scores: List.of(13),
+        bowling: false,
+        gameOver: false
+      }
+    )
+  })
+
+  it('only treats rolls in the same frame as a spare', () => {
+    expect(
+      gameReducer(
+        {
+          frames: List.of(
+            {
+              rolls: List([6, 4]),
+              strike: false,
+              spare: true,
+              finished: true
+            },
+            {
+              rolls: List([6]),
+              strike: false,
+              spare: false,
+              finished: false
+            }
+          ),
+          scores: List.of(16),
+          bowling: false,
+          gameOver: false
+        }, {
+          type: actions.BOWL_FINISHED,
+          results: {
+            down: 3
+          }
+        })
+    ).toEqual(
+      {
+        frames: List.of(
+          {
+            rolls: List([6, 4]),
+            strike: false,
+            spare: true,
+            finished: true
+          },
+          {
+            rolls: List([6, 3]),
+            strike: false,
+            spare: false,
+            finished: true
+          }
+        ),
+        scores: List.of(16, 25),
+        bowling: false,
+        gameOver: false
+      }
+    )
+  })
+
+  it('adds the score of the next two rolls onto the strike frame as a bonus score', () => {
+    expect(
+      gameReducer(
+        {
+          frames: List.of(
+            {
+              rolls: List([10]),
+              strike: true,
+              spare: false,
+              finished: true
+            },
+            {
+              rolls: List([4]),
+              strike: false,
+              spare: false,
+              finished: false
+            }
+          ),
+          scores: List(),
+          bowling: false,
+          gameOver: false
+        }, {
+          type: actions.BOWL_FINISHED,
+          results: {
+            down: 3
+          }
+        })
+    ).toEqual(
+      {
+        frames: List.of(
+          {
+            rolls: List([10]),
+            strike: true,
+            spare: false,
+            finished: true
+          },
+          {
+            rolls: List([4, 3]),
+            strike: false,
+            spare: false,
+            finished: true
+          }
+        ),
+        scores: List.of(17, 24),
+        bowling: false,
+        gameOver: false
+      }
+    )
+  })
+
+   it('does not add the strike score immediately', () => {
+    expect(
+      gameReducer(
+        {
+          frames: List.of(
+            {
+              rolls: List([4, 4]),
+              strike: false,
+              spare: false,
+              finished: true
+            }
+          ),
+          scores: List.of(8),
+          bowling: false,
+          gameOver: false
+        }, {
+          type: actions.BOWL_FINISHED,
+          results: {
+            down: 10
+          }
+        })
+    ).toEqual(
+      {
+        frames: List.of(
+          {
+            rolls: List([4, 4]),
+            strike: false,
+            spare: false,
+            finished: true
+          },
+          {
+            rolls: List([10]),
+            strike: true,
+            spare: false,
+            finished: true
+          }
+        ),
+        scores: List.of(8),
+        bowling: false,
+        gameOver: false
+      }
+    )
+  })
+
+  it('has a score of 300 for a perfect game', () => {
+    let strikeFrame = {
+      rolls: List([10]),
+      strike: true,
+      spare: false,
+      finished: true
+    }
+
+    expect(
+      gameReducer(
+        {
+          frames: List.of(
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame
+          ),
+          scores: List.of(30, 60, 90, 120, 150, 180, 210, 240, 270),
+          bowling: false,
+          gameOver: false
+        }, {
+          type: actions.BOWL_FINISHED,
+          results: {
+            down: 10
+          }
+        })
+    ).toEqual(
+      {
+        frames: List.of(
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame
+        ),
+        scores: List.of(30, 60, 90, 120, 150, 180, 210, 240, 270, 300),
+        bowling: false,
+        gameOver: false
+      }
+    )
+  })
+
+  it('allows an extra roll if a spare is scored in the last frame', () => {
+    let strikeFrame = {
+      rolls: List([10]),
+      strike: true,
+      spare: false,
+      finished: true
+    }
+
+    expect(
+      gameReducer(
+        {
+          frames: List.of(
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            strikeFrame,
+            {
+              rolls: List([6, 4]),
+              strike: false,
+              spare: true,
+              finished: true
+            }
+          ),
+          scores: List.of(30, 60, 90, 120, 150, 180, 210, 240),
+          bowling: false,
+          gameOver: false
+        }, {
+          type: actions.FRAME_FINISHED
+        })
+    ).toEqual(
+      {
+        frames: List.of(
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          strikeFrame,
+          {
+            rolls: List([6, 4]),
+            strike: false,
+            spare: true,
+            finished: true
+          }
+        ),
+        scores: List.of(30, 60, 90, 120, 150, 180, 210, 240),
+        bowling: false,
+        gameOver: false
+      }
+    )
+  })
 
   it('resets the state on a new game', () => {
     let strikeFrame = {
